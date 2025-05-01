1e5c5f60da5800e41d50af0634ab8fd837f51a48
eric-hernandezgarcia_tvlport <eric.hernandezgarcia@travelport.com>
2025-05-02 01:02:21 +0200

    TimePicker with errors

---
+++ src/stories/TimePicker.stories.js
@@ -0,0 +1,25 @@
+import { TimePicker } from "fluight-core";
+
+export default {
+  title: "Example/TimePicker",
+  component: TimePicker,
+  parameters: {
+    docs: {
+      description: {
+        component: "This component allows you to pick any time you want",
+      },
+    },
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const Primary = {
+  args:{
+    timeDefault: Date,
+    dataTestId: "time-component",
+    onTimeSelected: (selectedTime) => {
+      console.log("Time Selected:", selectedTime);
+    },
+  }
+};
