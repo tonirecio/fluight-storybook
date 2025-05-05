626477da3f0c2084b2be597626f7225ae609d56b
eric-hernandezgarcia_tvlport <eric.hernandezgarcia@travelport.com>
2025-05-05 10:01:43 +0200

    DatePicker

---
+++ src/stories/DatePicker.stories.js
@@ -0,0 +1,25 @@
+import { DatePicker } from "fluight-core";
+
+export default {
+  title: "Example/DatePicker",
+  component: DatePicker,
+  parameters: {
+    docs: {
+      description: {
+        component: "This component allows you to select a date using a native date input.",
+      },
+    },
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const Primary = {
+  args: {
+    dateDefault: new Date(),
+    dataTestId: "date-picker",
+    onDateSelected: (selectedDate) => {
+      console.log("Fecha seleccionada:", selectedDate);
+    },
+  },
+};
