6586922a540bb7c71227c8ae7c1f3da384f3aac6
Toni Recio <6155833+tonirecio@users.noreply.github.com>
2025-05-06 09:08:22 +0000

    GH actions fixes

--- src/stories/LocationPicker.stories.js
+++ src/stories/LocationPicker.stories.js
@@ -1,5 +1,4 @@
 import { LocationPicker } from "fluight-core";
-import locationData from "../../../fluight-core/src/components/data/LocationPicker.json";
 
 export default {
   title: "Form/LocationPicker",
@@ -7,7 +6,8 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: "This component allows you to select a location and filters results while typing.",
+        component:
+          "This component allows you to select a location and filters results while typing.",
       },
     },
     layout: "centered",
@@ -17,18 +17,10 @@ export default {
 
 export const Location = {
   args: {
-    previousSearches: locationData.slice(0, 5),
-    selectedLocation: locationData[0],
+    previousSearches: [],
+    selectedLocation: null,
     placeholderValue: "Origin",
     dataTestId: "location-picker",
-    onGetData: (filter) => {
-      return locationData.filter((item) =>
-        item.code.toLowerCase().includes(filter.toLowerCase()) ||
-        item.city.toLowerCase().includes(filter.toLowerCase()) ||
-        item.country.toLowerCase().includes(filter.toLowerCase()) ||
-        item.airportName.toLowerCase().includes(filter.toLowerCase())
-      );
-    },
     onLocationSelected: (location) => {
       console.log("Ubicación seleccionada:", location);
     },
---
+++ storybook-static/assets/CarriersFilter.stories-DobWpu1Q.js
@@ -0,0 +1,6 @@
+import{a as t}from"./index.esm-D1vCxKJO.js";import"./index-D4H_InIO.js";import"./jsx-runtime-BO8uF4Og.js";const l={title:"Form/CarriersFilter",component:t,parameters:{layout:"centered",docs:{description:{component:"This component allows you to select an airline from the deployable and choose if it is 'preferred', 'exclude', 'only' or 'all' at the moment of the search"}}},tags:["autodocs"]},e={args:{airlines:["IB","LO","LH","1I","1T","2B"],preference:["Preferred","Exclude","Only","All"]}};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    airlines: ["IB", "LO", "LH", "1I", "1T", "2B"],
+    preference: ["Preferred", "Exclude", "Only", "All"]
+  }
+}`,...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const c=["Primary"];export{e as Primary,c as __namedExportsOrder,l as default};
